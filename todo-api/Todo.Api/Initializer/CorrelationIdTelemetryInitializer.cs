ac3ef2c293232b64dd6fe514d22dfba57a5505b1
William Klein <klein.william@outlook.com>
2019-04-14 16:43:20 +0200

    feat(initializer): add initializer

---
+++ todo-api/Todo.Api/Initializer/CorrelationIdTelemetryInitializer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Http;
+
+namespace Todo.Initializer
+{
+    public class CorrelationIdTelemetryInitializer : ITelemetryInitializer
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public CorrelationIdTelemetryInitializer(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            var headers = this.httpContextAccessor?.HttpContext?.Request?.Headers;
+            string operationId = (headers != null && headers.ContainsKey("Request-Id")) ? headers["Request-Id"].ToString() : null;
+            if (operationId != null)
+            {
+                telemetry.Context.GlobalProperties["Operation Id"] = operationId;
+            }
+        }
+    }
+}
