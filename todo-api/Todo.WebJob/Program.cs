9d1df19178661562144ea96e2dc14196eb2ab67b
William Klein <klein.william@outlook.com>
2019-04-22 03:13:52 +0200

    refacto(job): use sdk v3 + remove hook url from sources

--- todo-api/Todo.WebJob/Program.cs
+++ todo-api/Todo.WebJob/Program.cs
@@ -1,42 +1,41 @@
-﻿using Microsoft.Azure.WebJobs;
-using Todo.Core.Clients;
+﻿using Todo.Core.Clients;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.ApplicationInsights;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace Todo.WebJob
 {
     class Program
     {
-        static void Main()
+        static async Task Main()
         {
-            var config = new JobHostConfiguration("UseDevelopmentStorage=true");
-
-            if (config.IsDevelopment)
-            {
-                config.UseDevelopmentSettings();
-            }
-
-            DoThings(config);
-
-            var host = new JobHost(config);
-            // The following code ensures that the WebJob will be running continuously
-            host.RunAndBlock();
-        }
-
-        private static void DoThings(JobHostConfiguration config)
-        {
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
-            serviceCollection.AddSingleton<Functions>();
-            serviceCollection.AddSingleton(new DiscordClient("https://discordapp.com/api/webhooks/568553400803786778/27l9zIA1D8BbntAhcNfkwzMJ47euGm95OyrA_j9qtItFPg3bC156lDOHOFwwtTJFfgzk"));
-
-            serviceCollection.AddLogging(builder =>
-            {
-                builder.AddConsole().AddApplicationInsights("41562bda-e522-49e1-a729-9c867ecf3547");
-            });
-
-            config.JobActivator = new ServiceCollectionJobActivator(serviceCollection);
+            await new HostBuilder()
+               .ConfigureHostConfiguration(builder =>
+               {
+                   builder.SetBasePath(Directory.GetCurrentDirectory())
+                       .AddJsonFile("appsettings.json", false, true);
+#if DEBUG
+                   builder.AddUserSecrets<Program>();
+#endif
+               })
+               .ConfigureLogging((context, builder) =>
+               {
+                   builder.AddConfiguration(context.Configuration.GetSection("Logging"))
+                          .AddApplicationInsights(context.Configuration["ApplicationInsights:InstrumentationKey"]);
+#if DEBUG
+                   builder.AddConsole();
+#endif
+               })
+               .ConfigureServices((context, services) =>
+               {
+                   services.AddSingleton<Functions>();
+                   services.AddSingleton(new DiscordClient(context.Configuration["DiscordWebhook"]));
+               })
+               .ConfigureWebJobs(builder => builder.AddAzureStorage())
+               .RunConsoleAsync();
         }
     }
 }
